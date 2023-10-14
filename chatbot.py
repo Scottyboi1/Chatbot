859fb0c22022b7590696feb753c9e6515a443a45
OzBirdog <ozbirdett@gmail.com>
2023-10-14 11:08:40 -0500

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -31,51 +31,83 @@ def get_response(grade_level, subject):
     }
     response = model.predict(prompt, **parameters)
     response_text.set(response.text)
+    
+    # Display the response in the Text widget
+    response_display.config(state="normal")
+    response_display.delete(1.0, "end")
+    response_display.insert("end", response.text)
+    response_display.config(state="disabled")
 
 # Create the main window
 window = tk.Tk()
 window.title("Chatbot Tutor")
 
-# Create an input entry field
-input_label = tk.Label(window, text="Enter your question:")
-input_label.pack()
-input_entry = tk.Entry(window, width=50)
-input_entry.pack()
+# Set a fixed window size
+window.geometry("800x400")  # Set your desired size
 
-# Create radio buttons to select the grade level
-grade_level = tk.StringVar()
-grade_level.set("middle")  # Default to middle school
+# Disable resizing
+window.resizable(0, 0)
 
-elementary_button = tk.Radiobutton(window, text="Elementary School", variable=grade_level, value="elementary")
-middle_button = tk.Radiobutton(window, text="Middle School", variable=grade_level, value="middle")
-high_button = tk.Radiobutton(window, text="High School", variable=grade_level, value="high")
+# Create a sidebar frame for subject and grade level selection
+sidebar_frame = tk.Frame(window)
+sidebar_frame.grid(row=0, column=0, padx=10, pady=10, sticky="n")
 
-elementary_button.pack()
-middle_button.pack()
-high_button.pack()
+# Create a subject selection frame inside the sidebar
+subject_frame = tk.Frame(sidebar_frame)
+subject_frame.grid(row=0, column=0)
+
+subject_label = tk.Label(subject_frame, text="Select Subject")
+subject_label.grid(row=0, column=0, sticky="w")
 
 # Create radio buttons to select the subject
 subject = tk.StringVar()
 subject.set("science")  # Default to science
 
-science_button = tk.Radiobutton(window, text="Science", variable=subject, value="science")
-math_button = tk.Radiobutton(window, text="Math", variable=subject, value="math")
-english_button = tk.Radiobutton(window, text="English", variable=subject, value="english")
-history_button = tk.Radiobutton(window, text="History", variable=subject, value="history")  # Add History radio button
+science_button = tk.Radiobutton(subject_frame, text="Science", variable=subject, value="science")
+math_button = tk.Radiobutton(subject_frame, text="Math", variable=subject, value="math")
+english_button = tk.Radiobutton(subject_frame, text="English", variable=subject, value="english")
+history_button = tk.Radiobutton(subject_frame, text="History", variable=subject, value="history")  # Add History radio button
 
-science_button.pack()
-math_button.pack()
-english_button.pack()
-history_button.pack()  # Pack the History radio button
+science_button.grid(row=1, column=0, sticky="w")
+math_button.grid(row=2, column=0, sticky="w")
+english_button.grid(row=3, column=0, sticky="w")
+history_button.grid(row=4, column=0, sticky="w")
 
-# Create a button to get the response
-get_response_button = tk.Button(window, text="Get Response", command=lambda: get_response(grade_level.get(), subject.get()))
-get_response_button.pack()
+# Create a grade level selection frame inside the sidebar
+grade_level_frame = tk.Frame(sidebar_frame)
+grade_level_frame.grid(row=1, column=0, pady=(20, 0))
 
-# Create a label to display the response
+grade_level_label = tk.Label(grade_level_frame, text="Select Grade Level")
+grade_level_label.grid(row=0, column=0, sticky="w")
+
+# Create radio buttons to select the grade level
+grade_level = tk.StringVar()
+grade_level.set("middle")  # Default to middle school
+
+elementary_button = tk.Radiobutton(grade_level_frame, text="Elementary School", variable=grade_level, value="elementary")
+middle_button = tk.Radiobutton(grade_level_frame, text="Middle School", variable=grade_level, value="middle")
+high_button = tk.Radiobutton(grade_level_frame, text="High School", variable=grade_level, value="high")
+
+elementary_button.grid(row=1, column=0, sticky="w")
+middle_button.grid(row=2, column=0, sticky="w")
+high_button.grid(row=3, column=0, sticky="w")
+
+# Create an input entry field on the main window
+input_entry = tk.Entry(window, width=50)
+input_entry.grid(row=0, column=1, padx=10, pady=10, sticky="n")
+
+# Create a button to get the response next to the menus
+get_response_button = tk.Button(sidebar_frame, text="Get Response", command=lambda: get_response(grade_level.get(), subject.get()))
+get_response_button.grid(row=2, column=0, padx=10, pady=10, sticky="w")
+
+# Create a Text widget to display the response under the entry field
 response_text = tk.StringVar()
-response_label = tk.Label(window, textvariable=response_text)
-response_label.pack()
+response_label = tk.Label(window, text="Response:")
+response_label.grid(row=1, column=0, padx=10, pady=10, sticky="w")
+
+response_display = tk.Text(window, height=10, width=50)
+response_display.grid(row=0, column=1, padx=10, pady=10, sticky="w")
+response_display.config(state="disabled")
 
 # Start the Tkinter main loop
 window.mainloop()
